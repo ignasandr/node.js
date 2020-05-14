9729a0de9ddf73611134f43e7b5457d6c16f6602
Ignas Andriuškevičius <63236361+ignasandr@users.noreply.github.com>
2020-05-14 22:20:21 +0300

    serve up static css

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('static'));
+
 
 app.get('/', function(req, res){
   res.render('index.ejs');
